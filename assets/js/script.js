59ab8861dbc0c06587da373feff916dcfa56067c
Dima <idooldima@gmail.com>
2021-03-23 11:27:01 +0200

    ready

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+function maxValue(a,b){
+    return a > b ? a : b;
+}
+
+console.log(maxValue(5,2))
+
+function minValue(a,b){
+    return a <= b ? a : b;
+}
+
+console.log(minValue(5,2))
+
+
+function parity(a){
+        return a % 2 === 0 ? "Even" : "Odd";
+}
+
+console.log(parity(245))
+
+
